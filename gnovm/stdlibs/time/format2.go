e51306d386828062abb6740783efc17287133418
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-10-23 16:02:51 +0200

    chore: garbage files

---
+++ gnovm/stdlibs/time/format2.go
@@ -0,0 +1,26 @@
+package time2
+
+var longMonthNames = []string{
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+}
+
+var longDayNames = []string{
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+}
